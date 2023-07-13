1cd8c4dbd2c76da2b8aa6d3a3677b1139325949f
imvicky0011 <kumarvikku0011@gmail.com>
2023-07-13 13:57:44 +0530

    adding state definations

--- public/js/store.js
+++ public/js/store.js
@@ -6,3 +6,42 @@ let state = {
     allowConnectionsFromStrangers: false,
     screenSharingActive: false
 }
+
+export const setSocketId = (socketId) => {
+    state = {
+        ...state,
+        socketId,
+    }
+}
+
+export const setLocalStream = (stream) => {
+    state = {
+        ...state,
+        localStream: stream
+    }
+}
+
+export const setAllowConnectionsFromStrangers = (allowConnections) => {
+    state = {
+        ...state,
+        allowConnectionsFromStrangers: allowConnections
+    }
+}
+
+export const setScreenSharingActive = (screenSharingActive) => {
+    state = {
+        ...state,
+        screenSharingActive
+    }
+}
+
+export const setScreenSharingStream = (stream) => {
+    state = {
+        ...state,
+        screenSharingStream: stream
+    }
+}
+
+export const getState = () => {
+    return state
+}
